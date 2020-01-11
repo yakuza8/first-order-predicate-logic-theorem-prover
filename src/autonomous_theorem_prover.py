b5192e09b63c692f3913ec5da181e9c0315b8f92
berat <berat.cankar@gmail.com>
2020-01-11 22:14:58 +0300

    Add docstring for pseudo code of resolution

--- src/autonomous_theorem_prover.py
+++ src/autonomous_theorem_prover.py
@@ -1,6 +1,37 @@
 import argparse
 
+from src import ProblemState
 from src.input_parser import InputParser
+
+
+class AutonomousTheoremProver(object):
+
+    def __init__(self, _problem_state: ProblemState):
+        self.problem_state = _problem_state
+
+    def prove(self):
+        """
+        Autonomous Theorem Prover
+        =========================
+        Pseudo code for procedure of resolution is explained
+
+        Symbols:
+            1) KB: Knowledge base
+            2) :math:`{\\alpha}`: Predicates to be proved
+
+        Aim: Prove KB -> :math:`{\\alpha}`
+
+        Code:
+        * CLAUSES <- CONVERT_TO_CNF( KB + :math:`{\\alpha}` )
+        * while EMPTY_RESOLVENT not in CLAUSES do
+            * select two distinct clauses {c_i} and {c_j} in CLAUSES
+            * compute a resolvent {r_ij} of {c_i} and {c_j}
+            * CLAUSES <- CLAUSES + {r_ij}
+        * end while
+        * return satisfaction
+        """
+        pass
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -13,4 +44,5 @@ if __name__ == '__main__':
 
     # Parse problem state
     problem_state = InputParser.parse(_file)
-    print()
+    # Prove the theorem
+    AutonomousTheoremProver(problem_state).prove()
