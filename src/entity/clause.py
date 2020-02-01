7ca75610287ec6b5916d8ca4096b6f99ba9226e7
berat <berat.cankar@gmail.com>
2020-02-01 16:52:07 +0300

    Add subsumption initial checks

--- src/entity/clause.py
+++ src/entity/clause.py
@@ -1,5 +1,6 @@
 import itertools
 import unittest
+from enum import Enum
 from typing import List, Optional
 
 from src import Predicate
@@ -7,10 +8,19 @@ from src.entity import children_entity_parser
 from src.most_general_unifier import MostGeneralUnifier
 
 
+class Subsumption(Enum):
+    LEFT_CLAUSE_SUBSUMES = 1
+    RIGHT_CLAUSE_SUBSUMES = 2
+    NO_SUBSUMPTION = 3
+
+
 class Clause(object):
     def __init__(self, predicates: List[Optional[Predicate]]):
         self.predicates = predicates
         self.predicates = sorted(self.predicates, key=lambda predicate: predicate.get_name())
+
+    def get_clause_length(self):
+        return len(self.predicates)
 
     def has_tautology(self) -> bool:
         """
@@ -30,6 +40,15 @@ class Clause(object):
         # If not achieved any tautology, it means we have no tautology
         return False
 
+    def does_subsume(self, other: 'Clause') -> bool:
+        # If no meet naming and negation match as a subset then immediately return False since subsumption cannot occur
+        fast_check_result = Clause._fast_check_by_negation_and_name(self, other)
+        if fast_check_result:
+            return True
+        else:
+            # TODO Add condition where fast check fails
+            return False
+
     @staticmethod
     def _predicate_separator_by_sign(predicates):
         non_negated, negated = [], []
@@ -37,12 +56,28 @@ class Clause(object):
             (non_negated, negated)[predicate.is_negated].append(predicate)
         return non_negated, negated
 
+    @staticmethod
+    def _fast_check_by_negation_and_name(clause1: 'Clause', clause2: 'Clause') -> bool:
+        clause1 = set(map(lambda predicate: (predicate.is_negated, predicate.get_name()), clause1.predicates))
+        clause2 = set(map(lambda predicate: (predicate.is_negated, predicate.get_name()), clause2.predicates))
+        return clause1.issubset(clause2)
+
 
 class ClauseUnitTest(unittest.TestCase):
 
     @staticmethod
     def _predicate_parser(predicates):
         return [Predicate.build(predicate) for predicate in children_entity_parser(predicates)]
+
+    def test_get_predicate_length(self):
+        clause = Clause([])
+        self.assertEqual(0, clause.get_clause_length())
+
+        clause = Clause(ClauseUnitTest._predicate_parser('p(y)'))
+        self.assertEqual(1, clause.get_clause_length())
+
+        clause = Clause(ClauseUnitTest._predicate_parser('p(y),q(y, A),r(A)'))
+        self.assertEqual(3, clause.get_clause_length())
 
     def test_has_tautology_empty_list(self):
         clause = Clause([])
@@ -84,3 +119,52 @@ class ClauseUnitTest(unittest.TestCase):
 
         clause = Clause(ClauseUnitTest._predicate_parser('p(x, r(ABC, k)),q(y, A),r(A),~p(u, r(b, k))'))
         self.assertTrue(clause.has_tautology())
+
+    def test_fast_check_valid(self):
+        clause1 = Clause(ClauseUnitTest._predicate_parser('p(y)'))
+        clause2 = Clause(ClauseUnitTest._predicate_parser('q(z),p(v)'))
+        self.assertTrue(Clause._fast_check_by_negation_and_name(clause1, clause2))
+
+        clause1 = Clause(ClauseUnitTest._predicate_parser('p(y)'))
+        clause2 = Clause(ClauseUnitTest._predicate_parser('q(z),~p(v)'))
+        self.assertFalse(Clause._fast_check_by_negation_and_name(clause1, clause2))
+
+        clause1 = Clause(ClauseUnitTest._predicate_parser('p(y)'))
+        clause2 = Clause(ClauseUnitTest._predicate_parser('q(z),p(k(l, ABC))'))
+        self.assertTrue(Clause._fast_check_by_negation_and_name(clause1, clause2))
+
+        clause1 = Clause(ClauseUnitTest._predicate_parser('p(y)'))
+        clause2 = Clause(ClauseUnitTest._predicate_parser('q(z),~p(k(l, ABC))'))
+        self.assertFalse(Clause._fast_check_by_negation_and_name(clause1, clause2))
+
+        clause1 = Clause(ClauseUnitTest._predicate_parser('p(y)'))
+        clause2 = Clause(ClauseUnitTest._predicate_parser('q(z),p(ABC, ACB, BAC, BCA, CAB, CBA)'))
+        self.assertTrue(Clause._fast_check_by_negation_and_name(clause1, clause2))
+
+        clause1 = Clause(ClauseUnitTest._predicate_parser('p(y)'))
+        clause2 = Clause(ClauseUnitTest._predicate_parser('q(z),~p(ABC, ACB, BAC, BCA, CAB, CBA)'))
+        self.assertFalse(Clause._fast_check_by_negation_and_name(clause1, clause2))
+
+        clause1 = Clause(ClauseUnitTest._predicate_parser('p(y)'))
+        clause2 = Clause(ClauseUnitTest._predicate_parser('q(z),p(ABC, ACB, BAC, BCA, CAB, CBA)'))
+        self.assertTrue(Clause._fast_check_by_negation_and_name(clause1, clause2))
+
+        clause1 = Clause(ClauseUnitTest._predicate_parser('p(y),~q(x)'))
+        clause2 = Clause(ClauseUnitTest._predicate_parser('q(z),p(ABC, ACB, BAC, BCA, CAB, CBA)'))
+        self.assertFalse(Clause._fast_check_by_negation_and_name(clause1, clause2))
+
+        clause1 = Clause(ClauseUnitTest._predicate_parser('p(y),q(x)'))
+        clause2 = Clause(ClauseUnitTest._predicate_parser('q(z),p(ABC, ACB, BAC, BCA, CAB, CBA)'))
+        self.assertTrue(Clause._fast_check_by_negation_and_name(clause1, clause2))
+
+        clause1 = Clause(ClauseUnitTest._predicate_parser('p(y),q(x),z(m)'))
+        clause2 = Clause(ClauseUnitTest._predicate_parser('q(z),p(ABC, ACB, BAC, BCA, CAB, CBA)'))
+        self.assertFalse(Clause._fast_check_by_negation_and_name(clause1, clause2))
+
+        clause1 = Clause(ClauseUnitTest._predicate_parser('p(y),p(x),p(o)'))
+        clause2 = Clause(ClauseUnitTest._predicate_parser('q(z),p(ABC, ACB, BAC, BCA, CAB, CBA)'))
+        self.assertTrue(Clause._fast_check_by_negation_and_name(clause1, clause2))
+
+
+    def test_subsumption(self):
+        pass
